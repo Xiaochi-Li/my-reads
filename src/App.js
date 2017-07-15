f2a3a6061cb8109eae02ec2b926d63160510377d
Unknown <294188719@qq.com>
2017-07-15 18:19:15 +0800

    build the state

--- src/App.js
+++ src/App.js
@@ -6,34 +6,35 @@ import BooksShelf from './BooksShelf.js'
 
 class App extends Component {
   state = {
-    screen: false
+    books: [],
+    screen : false,
+    booksReading : [{
+      backgroundImageUrl:'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+      bookTitle : 'To Kill a Mockingbird',
+      bookAuthors : 'Harper Lee',
+    }],
+    booksWantToRead : [{
+      backgroundImageUrl:'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
+      bookTitle : `Ender's Game`,
+      bookAuthors : 'Orson Scott Card',
+    }],
+    booksRead : [{
+      backgroundImageUrl:'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
+      bookTitle : '1776',
+      bookAuthors : 'David McCullough',
+    }]
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((extractedBooks) => {
+      this.setState({
+        books : extractedBooks
+      })
+    })
+    console.log(this.state);
   }
 
   render(){
-    const booksReading = [
-       {
-        backgroundImageUrl:'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
-        bookTitle : 'To Kill a Mockingbird',
-        bookAuthors : 'Harper Lee',
-      }
-    ];
-
-    const booksWantToRead = [
-       {
-        backgroundImageUrl:'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
-        bookTitle : `Ender's Game`,
-        bookAuthors : 'Orson Scott Card',
-      }
-    ];
-
-    const booksRead = [
-       {
-        backgroundImageUrl:'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")',
-        bookTitle : '1776',
-        bookAuthors : 'David McCullough',
-      }
-    ];
-
 
     return(
     <div className="app">
@@ -57,15 +58,15 @@ class App extends Component {
           <div className="list-books-content">
             <BooksShelf
               bookShelfTitle = 'Currently to Read'
-              books = {booksReading}>
+              books = {this.state.booksReading}>
             </BooksShelf>
             <BooksShelf
               bookShelfTitle = 'Want to Read'
-              books = {booksWantToRead}>
+              books = {this.state.booksWantToRead}>
             </BooksShelf>
             <BooksShelf
               bookShelfTitle = 'Read'
-              books = {booksRead}>
+              books = {this.state.booksRead}>
             </BooksShelf>
           </div>
 
