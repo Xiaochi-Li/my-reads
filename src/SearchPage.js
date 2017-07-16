c0869a4473ce04b4443a49b7940e571299578c2e
Unknown <294188719@qq.com>
2017-07-16 14:46:53 +0800

    but fixed

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -20,30 +20,33 @@ class SearchPage extends Component{
     }
 
   componentDidMount(){
-      if(this.state.queryquery!=''){
+      if(this.state.queryquery){
         BooksAPI.search(this.state.query).then((extractedBook)=>(
             this.setState({resultBooks : extractedBook})
-    ))}
-    console.log(this.state.resultBooks)
+        ))
+        console.log('a')   
+    }
   }
 
   render() {
       return(
         <div className="search-books">
             <div className="search-books-bar">
-            <Link className="close-search" to="/">Close</Link>
-            <div className="search-books-input-wrapper">
-              <input 
-              type="text" 
-              placeholder="Search by title or author"
-              value={this.state.query}
-              onChange={(event)=> this.updateBookQuery(event.target.value)}/>
+                <Link className="close-search" to="/">Close</Link>
+                <div className="search-books-input-wrapper">
+                <input 
+                type="text" 
+                placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={(event)=> this.updateBookQuery(event.target.value)}/>
+                </div>
             </div>
-        </div>
-          {/* 
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div> */}
+
+            <div className="search-books-results">
+            <ol className="books-grid">
+                {this.state.resultBooks.map((book)=>(<li>a </li>))}
+            </ol>
+            </div> 
         </div>
       )
   }
